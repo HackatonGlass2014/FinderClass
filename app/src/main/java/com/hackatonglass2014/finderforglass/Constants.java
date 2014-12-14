cae61d8d5aa058176eb982a299f0b97640107f6f
Anton Rozhkov <aurozhkov@gmail.com>
2014-12-14 18:28:27 +0600

    Remove token

--- app/src/main/java/com/hackatonglass2014/finderforglass/Constants.java
+++ app/src/main/java/com/hackatonglass2014/finderforglass/Constants.java
@@ -4,6 +4,6 @@ package com.hackatonglass2014.finderforglass;
  * Created by anton on 14.12.14.
  */
 public class Constants {
-    public static final String TOKEN = "PU5YcuAMcSmshzFkWvzlKZJZyY1Wp1MREknjsn2lKPu5urU6b3";
+    public static final String TOKEN = "put-your-api-key-here";
     public static final String PHOTO_PATH_EXTRA = "photo_path";
 }
