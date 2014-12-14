2aab880b764a4d49d8282ac7f34dc441990a9f6b
Anton Rozhkov <aurozhkov@gmail.com>
2014-12-14 12:56:25 +0600

    add camera request

--- app/src/main/java/com/hackatonglass2014/finderforglass/MainActivity.java
+++ app/src/main/java/com/hackatonglass2014/finderforglass/MainActivity.java
@@ -2,18 +2,36 @@ package com.hackatonglass2014.finderforglass;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.FileObserver;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 
+import com.google.android.glass.content.Intents;
 import com.google.android.glass.media.Sounds;
 import com.google.android.glass.widget.CardBuilder;
 import com.google.android.glass.widget.CardScrollAdapter;
 import com.google.android.glass.widget.CardScrollView;
+import com.hackatonglass2014.finderforglass.network.ApiClientProvider;
+import com.hackatonglass2014.finderforglass.network.UploadResponse;
+
+import java.io.File;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.mime.TypedFile;
 
 public class MainActivity extends Activity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int CAMERA_REQUEST_CODE = 100;
 
     private CardScrollView mCardScroller;
 
@@ -53,6 +71,7 @@ public class MainActivity extends Activity {
         mCardScroller.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), CAMERA_REQUEST_CODE);
                 AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
                 am.playSoundEffect(Sounds.DISALLOWED);
             }
@@ -64,6 +83,68 @@ public class MainActivity extends Activity {
     protected void onResume() {
         super.onResume();
         mCardScroller.activate();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            String thumbnailPath = data.getStringExtra(Intents.EXTRA_THUMBNAIL_FILE_PATH);
+            String picturePath = data.getStringExtra(Intents.EXTRA_PICTURE_FILE_PATH);
+
+            processPictureWhenReady(picturePath);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void processPictureWhenReady(final String picturePath) {
+        final File pictureFile = new File(picturePath);
+        if (pictureFile.exists()) {
+            processPicture(pictureFile);
+        } else {
+            final File parentDirectory = pictureFile.getParentFile();
+            FileObserver observer = new FileObserver(parentDirectory.getPath(),
+                    FileObserver.CLOSE_WRITE | FileObserver.MOVED_TO) {
+                private boolean isFileWritten;
+
+                @Override
+                public void onEvent(int event, String path) {
+                    if (!isFileWritten) {
+                        File affectedFile = new File(parentDirectory, path);
+                        isFileWritten = affectedFile.equals(pictureFile);
+
+                        if (isFileWritten) {
+                            stopWatching();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    processPictureWhenReady(picturePath);
+                                }
+                            });
+                        }
+                    }
+                }
+            };
+            observer.startWatching();
+        }
+    }
+
+    private void processPicture(File pictureFile) {
+        Log.v(TAG, "photo complete");
+        /*ApiClientProvider.provideApiClient().uploadPhoto(
+                "en",
+                new TypedFile("image/*", pictureFile),
+                new Callback<UploadResponse>() {
+                    @Override
+                    public void success(UploadResponse uploadResponse, Response response) {
+                        Log.v(TAG, "photo uploaded");
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        Log.v(TAG, "error");
+                    }
+                });*/
     }
 
     @Override
